ddd0995d0e236259171e75a5056baf6c0c27d974
Sontaya Rungsaard <sontaya16071997@gmail.com>
2021-07-01 23:42:10 +0700

    update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@ export default defineComponent({
   <div class="bg-gray-50">
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
       <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <span class="block text-green-400">GSON</span>
         <span class="block">Ready to dive in?</span>
         <span class="block text-indigo-600">Start your free trial today.</span>
       </h2>
